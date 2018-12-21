b3642fd96bf835f10cc5e2e29c47567f9456a719
lebedev123 <shepka654@gmail.com>
2018-12-21 13:36:52 +0000

    Revert "get labs"
    This reverts commit 758535890325c83f022948f3fb73646920a98ad4.

---
+++ icmp.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+for host in available_ip_addresses:
+    print(ping -i 1 host)
+
---
+++ labs/handleError.py
@@ -0,0 +1,8 @@
+try:
+    a = input('Введите первое число: ')
+    b = input('Введите второе число: ')
+    print('Результат: ', int(a) / int(b))
+except ValueError:
+    print('Пожалуйста, вводите только числа')
+except ZeroDivisionError:
+    print('На ноль делить нельзя')
